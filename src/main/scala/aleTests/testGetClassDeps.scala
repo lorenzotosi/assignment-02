0c6c93dd887f175154e981750dd9516300b502b4
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-17 20:53:11 +0200

    Update testGetClassDeps.scala

--- src/main/scala/aleTests/testGetClassDeps.scala
+++ src/main/scala/aleTests/testGetClassDeps.scala
@@ -1,34 +1,35 @@
 package aleTests
 
-import lib.DependencyAnalyser
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
+import io.vertx.core.Vertx
+import lib.DependencyAnalyserLib.*
 
-import scala.concurrent.Await
 import java.io.File
 
 @main def runDependencyAnalysis(): Unit =
   val file = File("src/main/scala/aleTests/DummyClass.java") // Cambia questo path con il tuo file .java
   val analyser = DependencyAnalyser()
 
+  val v = Vertx.vertx()
+  v.deployVerticle(analyser)
+
   println("Analizzando: " + file.getAbsolutePath)
   println("TEST 1:")
   val futureReport = analyser.getClassDependencies(file)
-  val report = Await.result(futureReport, 10.seconds)
-  println("File: " + report.className)
 
-  println("Dipendenze trovate:")
-  report.depsList.foreach(dep => println("-" + dep))
+  val report = futureReport.onComplete(x =>
+    println("File: " + x.result().className)
 
-  println("TEST 2:")
-  println("Dipendenze trovate:")
-  val futureReport2 = analyser.getClassDependencies(file)
+    println("Dipendenze trovate:")
+    x.result().depsList.foreach(dep => println("-" + dep))
+    v.close()
+  )
 
+  val packageDir = new File("src/main/scala/aleTests/")
+  val packageReport = analyser.getPackageDependencies(packageDir)
+  packageReport.onComplete(x => x.result().printClasses())
   var i: Int = 0
-  futureReport2.onComplete {res => res.get.depsList.foreach(d => println("-" + d))} //non stampa perché termina prima il main duh
-  println(i)
 
-  while (i<100) //giusto per confermare la asincronità del metodo
+  while (i<1000)
     i = i+1
     println(i)
     Thread.sleep(1)
