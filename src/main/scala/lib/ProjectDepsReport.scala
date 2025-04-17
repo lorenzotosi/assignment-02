b470467cdf1950aa7610cd040f42437730263861
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-17 20:53:09 +0200

    Update ProjectDepsReport.scala

--- src/main/scala/lib/ProjectDepsReport.scala
+++ src/main/scala/lib/ProjectDepsReport.scala
@@ -1,9 +1,9 @@
 package lib
 
+import lib.DependencyAnalyserLib.Report
+
 class ProjectDepsReport extends Report:
 
   override def depsList: Set[String] = ???
-
-  override def addDep(dependency: String): Unit = ???
 
 
