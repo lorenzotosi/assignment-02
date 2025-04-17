d3f7c56de9453ec3983ca7be72346de82e7ae3b9
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-17 20:53:06 +0200

    Update PackageDepsReport.scala

--- src/main/scala/lib/PackageDepsReport.scala
+++ src/main/scala/lib/PackageDepsReport.scala
@@ -1,7 +1,14 @@
 package lib
 
+import lib.DependencyAnalyserLib.Report
 
-class PackageDepsReport extends Report:
-  override def depsList: Set[String] = ???
+class PackageDepsReport(val packageName: String, val classes: List[ClassDepsReport]) extends Report:
 
-  override def addDep(dependency: String): Unit = ???
+  override def depsList: Set[String] = classes.flatMap(x => x.depsList).toSet
+
+  def printClasses(): Unit = {
+    classes.foreach(c => {
+      println("Class name: ".concat(c.className))
+      c.depsList.foreach(println(_))
+    })
+  }
