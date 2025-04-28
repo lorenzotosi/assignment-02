10b5b6b65a09d48e1bd579d7cc7e2a07a6a3659f
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-28 10:20:18 +0200

    uncomment tests

--- src/main/scala/aleTests/testGetClassDeps.scala
+++ src/main/scala/aleTests/testGetClassDeps.scala
@@ -6,7 +6,7 @@ import lib.DependencyAnalyserLib.*
 import java.io.File
 
 @main def runDependencyAnalysis(): Unit =
-  val file = File("src/main/java/pcd/ass02/MyClass.java") // Cambia questo path con il tuo file .java
+  val file = File("src/main/java/pcd/ass02/foopack/D.java") // Cambia questo path con il tuo file .java
   val analyser = DependencyAnalyser()
 
   val v = Vertx.vertx()
@@ -20,25 +20,25 @@ import java.io.File
     println("Dipendenze trovate:")
     x.result().printInformation()
   )
-//
-//  val packageDir = new File("src/main/scala/aleTests/")
-//  println("Analizzando: " + packageDir.getAbsolutePath)
-//  println("TEST 2:")
-//  val packageReport = analyser.getPackageDependencies(packageDir)
-//  packageReport.onComplete(x =>
-//    x.result().printInformation())
-//  var i: Int = 0
-//
-//  val projectDir = new File("src/main/java/")
-//  println("Analizzando: " + projectDir.getAbsolutePath)
-//  println("TEST 3:")
-//  val projectReport = analyser.getProjectDependencies(projectDir)
-//  projectReport.onComplete(
-//    x =>
-//      x.result().printInformation()
-//      v.close()
-//  )
 
+  val packageDir = new File("src/main/java/pcd/ass02/foopack")
+  println("Analizzando: " + packageDir.getAbsolutePath)
+  println("TEST 2:")
+  val packageReport = analyser.getPackageDependencies(packageDir)
+  packageReport.onComplete(x =>
+    x.result().printInformation())
+  //var i: Int = 0
+
+  val projectDir = new File("src/main/java/pcd/ass02/")
+  println("Analizzando: " + projectDir.getAbsolutePath)
+  println("TEST 3:")
+  val projectReport = analyser.getProjectDependencies(projectDir)
+  projectReport.onComplete(
+    x =>
+      x.result().printInformation()
+      v.close()
+  )
+//
 //  while (i<1000)
 //    i = i+1
 //    println(s"Eseguito dal thread: ${Thread.currentThread().getName}" + i)
