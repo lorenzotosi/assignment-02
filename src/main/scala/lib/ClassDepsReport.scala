7f1c06960b1bba5c3aef912b3ef22161f7613160
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-28 10:20:03 +0200

    update print

--- src/main/scala/lib/ClassDepsReport.scala
+++ src/main/scala/lib/ClassDepsReport.scala
@@ -7,8 +7,10 @@ import java.io.File
 class ClassDepsReport(val file: File, val depList: Set[String], val map: Map[String, Set[String]]) extends Report:
   override def depsList: Set[String] = depList
 
-  override def printInformation(): Unit =
+  override def printInformation(): Unit = {
+    println("  Nome file analizzato: " + file.getName)
     map.foreach((z, y) =>
-      println(z + ":")
-      y.foreach(x => println(x))
+      println("   " + z + ":")
+      if y.nonEmpty then y.foreach(x => println("    " + x)) else println("    -")
     )
+  }
