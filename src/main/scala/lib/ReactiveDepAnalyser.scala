424a96b5203863e4904c8798011e14438481a25e
Matteo Susca <matteo.susca@studio.unibo.it>
2025-05-03 16:54:14 +0200

    first (not working) test

---
+++ src/main/scala/lib/ReactiveDepAnalyser.scala
@@ -0,0 +1,63 @@
+package lib
+
+import com.github.javaparser.StaticJavaParser
+import com.github.javaparser.ast.CompilationUnit
+import io.reactivex.rxjava3.core.{Observable, Single}
+import java.io.File
+import scala.jdk.CollectionConverters.*
+
+
+object ReactiveDepAnalyser:
+
+	class ReactiveDependencyAnalyser:
+
+		def getClassDependencies(classSrcFile: File): Single[ClassDepsReport] =
+			Single.create(emitter => {
+				if !classSrcFile.isFile || !classSrcFile.getName.endsWith(".java") then
+					emitter.onError(new IllegalArgumentException("Il file non è un sorgente .java."))
+				else
+					try
+						val visitor = MyVoidVisitorAdapter()
+						val cu: CompilationUnit = StaticJavaParser.parse(classSrcFile)
+						visitor.visit(cu, null)
+						val map: Map[String, List[String]] = visitor.getMap
+						emitter.onSuccess(ClassDepsReport(classSrcFile, map))
+					catch
+						case ex: Exception =>
+							emitter.onError(new RuntimeException(s"Errore durante l'analisi del file ${classSrcFile.getName}: ${ex.getMessage}"))
+			})
+
+		def getPackageDependencies(packageSrcFolder: File): Single[PackageDepsReport] =
+			Single.create(emitter => {
+				if !packageSrcFolder.isDirectory then
+					emitter.onError(new IllegalArgumentException("Il percorso specificato non è un package."))
+				else
+					val files = packageSrcFolder.listFiles(f => f.isDirectory || (f.isFile && f.getName.endsWith(".java")))
+					val (directories, javaFiles) = files.partition(_.isDirectory)
+
+					val subPackages = directories.map(getPackageDependencies).toList
+					val classes = javaFiles.map(getClassDependencies).toList
+
+					Observable.fromIterable(subPackages.asJava)
+						.flatMapSingle(identity)
+						.toList
+						.flatMap(subPackagesList =>
+							Observable.fromIterable(classes.asJava)
+								.flatMapSingle(identity)
+								.toList
+								.map(classesList => PackageDepsReport(packageSrcFolder, subPackagesList.asScala.toList, classesList.asScala.toList))
+						).subscribe(emitter.onSuccess, emitter.onError)
+			})
+
+		def getProjectDependencies(projectSrcFolder: File): Single[ProjectDepsReport] =
+			Single.create(emitter => {
+				if !projectSrcFolder.isDirectory then
+					emitter.onError(new IllegalArgumentException("Il percorso specificato non è un progetto."))
+				else
+					val folders = projectSrcFolder.listFiles(_.isDirectory)
+					Observable.fromIterable(folders.toSeq.asJava)
+						.flatMapSingle(getPackageDependencies)
+						.toList
+						.map(packages => ProjectDepsReport(projectSrcFolder, packages.asScala.toList))
+						.subscribe(emitter.onSuccess, emitter.onError)
+			})
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,7 @@
 @main
 def runDependencyAnalyser(): Unit = {
-  val gui = Gui.createGui()
+  // val gui = Gui.createGui()
+  val gui = DepsGui.createGui()
   gui.pack()
   gui.centerOnScreen()
   gui.open()
