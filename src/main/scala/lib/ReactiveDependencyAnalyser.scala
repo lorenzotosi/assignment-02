c0d8ea55acef8c7803309ed4c30d9d272f67b08f
Matteo Susca <matteo.susca@studio.unibo.it>
2025-05-04 22:17:27 +0200

    NOT WORKING. using new approach

--- src/main/scala/lib/ReactiveDependencyAnalyser.scala
+++ src/main/scala/lib/ReactiveDependencyAnalyser.scala
@@ -4,11 +4,9 @@ import com.github.javaparser.StaticJavaParser
 import com.github.javaparser.ast.CompilationUnit
 import io.reactivex.rxjava3.core.{Single, Observable}
 import java.io.File
-import java.util.concurrent.Callable
 import scala.jdk.CollectionConverters._
 
 object ReactiveDependencyAnalyser:
-	// ... Resto delle definizioni rimane invariato ...
 
 	trait Analyzer:
 		def getClassDependencies(classSrcFile: File): Single[ClassDepsReport]
@@ -18,62 +16,52 @@ object ReactiveDependencyAnalyser:
 	class ReactiveDependencyAnalyser extends Analyzer:
 		private val scheduler = io.reactivex.rxjava3.schedulers.Schedulers.io()
 
-		override def getClassDependencies(classSrcFile: File): Single[ClassDepsReport] =
-			Single.fromCallable(() => {
+		override def getClassDependencies(classSrcFile: File): Single[ClassDepsReport] = {
+			println(s"Analizzando la classe: ${classSrcFile.getAbsolutePath}")
+			Single.fromCallable { () =>
 				if !classSrcFile.isFile || !classSrcFile.getName.endsWith(".java") then
 					throw new IllegalArgumentException("Il file non è un sorgente .java.")
 
-				val x = new MyVoidVisitorAdapter()
-				val cu = StaticJavaParser.parse(classSrcFile)
-				x.visit(cu, null)
-				ClassDepsReport(classSrcFile, x.getMap)
-			}).subscribeOn(scheduler)
+				val visitor = new MyVoidVisitorAdapter()
+				StaticJavaParser.parse(classSrcFile).accept(visitor, null)
+				ClassDepsReport(classSrcFile, visitor.getMap)
+			}.subscribeOn(scheduler)
+		}
 
 		override def getPackageDependencies(packageSrcFolder: File): Observable[PackageDepsReport] =
 			if !packageSrcFolder.isDirectory then
 				Observable.error(IllegalArgumentException("Il percorso specificato non è un package."))
 			else
-			Observable.defer(() => {
-				val allFiles = packageSrcFolder.listFiles.toList
-				val (directories, files) = allFiles.partition(_.isDirectory)
-		
-				val subPackagesObs = Observable.fromIterable(directories.asJava).flatMap(getPackageDependencies)
-				val classesObs = Observable.fromIterable(files.asJava).flatMap(f =>
-					getClassDependencies(f).toObservable
-				)
-		
-				// Converti i Single in Observable
-				Observable.zip(
-					subPackagesObs.toList.toObservable,
-					classesObs.toList.toObservable,
-					(subPackages: java.util.List[PackageDepsReport], classes: java.util.List[ClassDepsReport]) =>
-						PackageDepsReport(
-							packageSrcFolder,
-							subPackages.asScala.toList,
-							classes.asScala.toList
-						)
-				)
-			}).subscribeOn(scheduler)
+				Observable.defer { () =>
+					val (directories, files) = packageSrcFolder.listFiles.toList.partition(_.isDirectory)
+
+					val subPackagesObs = Observable.fromIterable(directories.asJava)
+						.flatMap(getPackageDependencies)
+
+					val classesObs = Observable.fromIterable(files.asJava)
+						.filter(_.getName.endsWith(".java"))
+						.flatMapSingle(getClassDependencies)
+
+					Observable.zip(
+						subPackagesObs.toList.toObservable,
+						classesObs.toList.toObservable,
+						(subPackages: java.util.List[PackageDepsReport], classes: java.util.List[ClassDepsReport]) =>
+							PackageDepsReport(
+								packageSrcFolder,
+								subPackages.asScala.toList,
+								classes.asScala.toList
+							)
+					)
+				}.subscribeOn(scheduler)
 
 		override def getProjectDependencies(projectSrcFolder: File): Observable[ProjectDepsReport] =
 			if !projectSrcFolder.isDirectory then
 				Observable.error(IllegalArgumentException("Il percorso specificato non è un progetto"))
 			else
-			Observable.defer(() => {
-				val packagesObs = Observable.fromIterable(
-					projectSrcFolder.listFiles
-						.filter(_.isDirectory)
-						.toList  // Converti a List Scala
-						.asJava  // Converti a Java Iterable
-				).flatMap(getPackageDependencies)
-
-				packagesObs
-					.toList
-					.map { packages =>
-						ProjectDepsReport(
-							projectSrcFolder,
-							packages.asScala.toList
-						)
-					}
-					.toObservable
-			}).subscribeOn(scheduler)
+				Observable.defer { () =>
+					Observable.fromIterable(projectSrcFolder.listFiles.toList.filter(_.isDirectory).asJava)
+						.flatMap(getPackageDependencies)
+						.toList
+						.map(packages => ProjectDepsReport(projectSrcFolder, packages.asScala.toList))
+						.toObservable
+				}.subscribeOn(scheduler)
