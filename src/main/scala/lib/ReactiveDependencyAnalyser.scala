7470add6b2e21a861046b69f504971377ab15e34
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-05-05 15:13:14 +0200

    using scheduler

--- src/main/scala/lib/ReactiveDependencyAnalyser.scala
+++ src/main/scala/lib/ReactiveDependencyAnalyser.scala
@@ -1,19 +1,11 @@
 package lib
 
 import com.github.javaparser.StaticJavaParser
-import com.github.javaparser.ast.CompilationUnit
-import io.reactivex.rxjava3.core.{Single, Observable}
+import io.reactivex.rxjava3.core.Observable
+
 import java.io.File
-import scala.jdk.CollectionConverters._
 
 object ReactiveDependencyAnalyser:
-
-  trait Analyzer:
-    def getClassDependencies(classSrcFile: File): Single[ClassDepsReport]
-
-    def getPackageDependencies(packageSrcFolder: File): Observable[PackageDepsReport]
-
-    def getProjectDependencies(projectSrcFolder: File): Observable[ProjectDepsReport]
 
   class ReactiveDependencyAnalyser:
 
@@ -22,6 +14,7 @@ object ReactiveDependencyAnalyser:
         throw new IllegalArgumentException("Il file non è un sorgente .java.")
       else
         println(s"Analizzando la classe: ${classSrcFile.getAbsolutePath}")
+        println(Thread.currentThread().getName)
         val visitor = new MyVoidVisitorAdapter()
         StaticJavaParser.parse(classSrcFile).accept(visitor, null)
         ClassDepsReport(classSrcFile, visitor.getMap)
@@ -32,7 +25,6 @@ object ReactiveDependencyAnalyser:
         Observable.error(IllegalArgumentException("Il percorso specificato non è una cartella."))
       else
         Observable.create(emitter => {
-          new Thread(() => {
             def searchFiles(dir: File): Unit = {
               val files = dir.listFiles
               if files != null then
@@ -52,44 +44,4 @@ object ReactiveDependencyAnalyser:
               emitter.onComplete()
             catch
               case ex: Exception => emitter.onError(ex)
-          }).start()
-        })
-
-
-    def getPackageDependencies(packageSrcFolder: File): Observable[PackageDepsReport] = ???
-    //      if !packageSrcFolder.isDirectory then
-    //        Observable.error(IllegalArgumentException("Il percorso specificato non è un package."))
-    //      else
-    //        Observable.defer { () =>
-    //          val (directories, files) = packageSrcFolder.listFiles.toList.partition(_.isDirectory)
-    //
-    //          val subPackagesObs = Observable.fromIterable(directories.asJava)
-    //            .flatMap(getPackageDependencies)
-    //
-    //          val classesObs = Observable.fromIterable(files.asJava)
-    //            .filter(_.getName.endsWith(".java"))
-    //            .flatMapSingle(getClassDependencies)
-    //
-    //          Observable.zip(
-    //            subPackagesObs.toList.toObservable,
-    //            classesObs.toList.toObservable,
-    //            (subPackages: java.util.List[PackageDepsReport], classes: java.util.List[ClassDepsReport]) =>
-    //              PackageDepsReport(
-    //                packageSrcFolder,
-    //                subPackages.asScala.toList,
-    //                classes.asScala.toList
-    //              )
-    //          )
-    //        }
-
-    def getProjectDependencies(projectSrcFolder: File): Observable[ProjectDepsReport] = ???
-//      if !projectSrcFolder.isDirectory then
-//        Observable.error(IllegalArgumentException("Il percorso specificato non è un progetto"))
-//      else
-//        Observable.defer { () =>
-//          Observable.fromIterable(projectSrcFolder.listFiles.toList.filter(_.isDirectory).asJava)
-//            .flatMap(getPackageDependencies)
-//            .toList
-//            .map(packages => ProjectDepsReport(projectSrcFolder, packages.asScala.toList))
-//            .toObservable
-//        }
+          })
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,12 +5,17 @@ import java.io.File
 
 @main
 def runDependencyAnalyser(): Unit = {
-  // val gui = Gui.createGui()
-
   val file = File("src/main/scala/aleTests/")
 
   val x = ReactiveDependencyAnalyser()
 
-  val y = x.getClassPaths(file).subscribe()
+  val scheduler = io.reactivex.rxjava3.schedulers.Schedulers.io()
 
+  val y = x.getClassPaths(file).subscribeOn(scheduler).subscribe(_.printInformation())
+  var i = 0
+  while (i < 1000) {
+    println(i + " " + Thread.currentThread().getName)
+    i = i + 1
+    Thread.sleep(100)
+  }
 }
